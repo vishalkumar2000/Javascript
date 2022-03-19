327662f6ed3841b947af1d49c0a012fef3299060
vishalkumar2000 <vishalrao026@gmail.com>
2022-03-19 16:23:02 +0530

    commit1

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,14 +1,58 @@
-let Input = document.querySelector('input');
-let btn = document.querySelector('button');
-let content = document.querySelector('.todos');
-let count =-1
-btn.addEventListener('click',function(){
-    count +=1;
-    let paragraph =document.createElement('p');
-    paragraph.innerHTML= Input.value
-    paragraph.setAttribute("key",count);
-    content.appendChild(paragraph);
-    paragraph.addEventListener('click',function(){
-        content.removeChild(paragraph);
-    })
-})
+// let Input = document.querySelector('input');
+// let btn = document.querySelector('button');
+// let content = document.querySelector('.todos');
+// let count =-1
+// btn.addEventListener('click',function(){
+//     count +=1;
+//     let paragraph =document.createElement('p');
+//     paragraph.innerHTML= Input.value
+//     paragraph.setAttribute("key",count);
+//     content.appendChild(paragraph);
+//     paragraph.addEventListener('click',function(){
+//         content.removeChild(paragraph);
+//     })
+// })
+let todoList;
+featchItem();
+document.querySelector("#btn1").addEventListener("click", () => {
+    loadItems(todoList);
+});
+document.querySelector("#btn2").addEventListener("click", () => {
+    let completedTodoList = todoList.filter(ele => ele.completed);
+    loadItems(completedTodoList);
+});
+document.querySelector("#btn3").addEventListener("click", () => {
+    let pendingTodoList = todoList.filter(ele => !ele.completed);
+    loadItems(pendingTodoList);
+});
+function featchItem() {
+    let xml = new XMLHttpRequest;
+    xml.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+    xml.onload = function () {
+        todoList = JSON.parse(this.responseText);
+        loadItems(todoList);
+    }
+    xml.send();
+}
+function loadItems(list) {
+    let todoArea = document.querySelector("#todoArea");
+    let html = "";
+    list.forEach(ele => {
+        let val
+        if (ele.completed) {
+            val = "Completed"
+        }
+        else {
+            val = "Pending"
+        }
+        html += `<div class="card" style="width: 18rem; margin: 10px">
+        <div class="card-body">
+            <h5 class="card-title">${ele.title}</h5>
+            <p class="card-text">User Id :  ${ele.userId}</p>
+            <p class="card-text">Id :  ${ele.id}</p>
+            <p class="card-text">Status : ${val}</p>
+        </div>
+        </div>`;
+    });
+    todoArea.innerHTML = html;
+}
